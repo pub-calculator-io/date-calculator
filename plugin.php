fd3a1c58d9698656c61b194d05ca873ae3ac594e
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:27:52 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Date Calculator by Calculator.iO
+Plugin Name: Date Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/date-calculator/
 Description: Free calculator to effortlessly evaluate the difference between dates. Easily add or subtract days from any date, with or without holidays and weekends.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Date Calculato
 
 function display_ci_date_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/date-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Date Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_date_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Date Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_date_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_date_calculator', 'display_ci_date_calculator' );
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ Free calculator to effortlessly evaluate the difference between dates. Easily ad
 
 ## Installation
 
-1. [Download](https://github.com/pub-calculator-io/age-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
+1. [Download](https://github.com/pub-calculator-io/date-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
 2. Upload the /date-calculator-master/ folder to the /wp-content/plugins/ directory.
 3. Activate the [Date Calculator](https://www.calculator.io/date-calculator/ "Date Calculator Homepage") plugin through the "Plugins" menu in WordPress.
 
